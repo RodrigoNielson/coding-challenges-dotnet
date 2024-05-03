360fddd74bb2f0f7c8bbc2908564127ad2cdedc0
maxtuist <digo_nielson@hotmail.com>
2024-05-03 20:56:28 -0300

    26. Remove Duplicates from Sorted Array

---
+++ src/leetcode-dotnet/Easy/RemoveDuplicatesFromSortedArray/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_dotnet.Easy.RemoveDuplicatesFromSortedArray;
+
+/// <see cref="https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/"/>
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        int j = 1;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[i-1])
+            {
+                nums[j++] = nums[i];
+            }
+        }
+
+        return j;
+    }
+}
---
+++ tests/leetcode-dotnet-test/Easy/RemoveDuplicatesFromSortedArrayTest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using leetcode_dotnet.Easy.RemoveDuplicatesFromSortedArray;
+using Xunit;
+
+namespace leetcode_dotnet_test.Easy;
+public class RemoveDuplicatesFromSortedArrayTest
+{
+    public static IEnumerable<object[]> GetData()
+    {
+        // Example 1
+        yield return new object[]
+        {
+            new int[] { 1, 1, 2 },
+            new int[] { 1, 2, 0 },
+            2
+        };
+
+        // Example 2:
+        yield return new object[]
+        {
+            new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 },
+            new int[] { 0, 1, 2, 3, 4, 0, 0, 0, 0, 0},
+            5
+        };
+    }
+
+    /// <summary>
+    /// 
+    /// 
+    /// </summary>
+    [Theory]
+    [MemberData(nameof(GetData))]
+    public void Test(int[] nums, int[] expectedNums, int expectedOutput)
+    {
+        var solution = new Solution();
+
+        var output = solution.RemoveDuplicates(nums);
+
+        output.Should().Be(expectedOutput);
+
+        nums[..expectedOutput].Should().BeEquivalentTo(expectedNums[..expectedOutput], opt => opt.WithStrictOrdering());
+    }
+}
