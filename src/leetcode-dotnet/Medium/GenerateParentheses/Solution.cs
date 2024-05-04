b92d646597423cfb389b267744826a0ade0d9f97
maxtuist <digo_nielson@hotmail.com>
2024-05-04 15:38:43 -0300

    22. Generate Parentheses

---
+++ src/leetcode-dotnet/Medium/GenerateParentheses/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_dotnet.Medium.GenerateParentheses;
+
+/// <see cref="https://leetcode.com/problems/generate-parentheses/"/>
+public class Solution
+{
+    public IList<string> GenerateParenthesis(int n)
+    {
+        var results = new List<string>();
+
+        GenerateParenthesisRecursive(n, 0, string.Empty, ref results); ;
+
+        return results;
+    }
+
+    public void GenerateParenthesisRecursive(int l, int openCount, string result, ref List<string> results)
+    {
+        if (l == 0 && openCount == 0)
+        {
+            results.Add(result);
+            return;
+        }
+
+        if (l > 0 && openCount >= 0)
+            GenerateParenthesisRecursive(l - 1, openCount + 1, result + "(", ref results);
+
+        if (openCount > 0)
+            GenerateParenthesisRecursive(l, openCount - 1, result + ")", ref results);
+    }
+}
---
+++ tests/leetcode-dotnet-test/Medium/GenerateParenthesesTest.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using leetcode_dotnet.Medium.GenerateParentheses;
+using Xunit;
+
+namespace leetcode_dotnet_test.Medium;
+public class GenerateParenthesesTest
+{
+    public static IEnumerable<object[]> GetData()
+    {
+        // Example 1
+        yield return new object[]
+        {
+            3,
+            new List<string> { "((()))", "(()())", "(())()", "()(())", "()()()" }
+        };
+
+        // Example 2:
+        yield return new object[]
+        {
+            1,
+            new List<string> { "()" }
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(GetData))]
+    public void Test(int n, List<string> expectedOutput)
+    {
+        var solution = new Solution();
+
+        var output = solution.GenerateParenthesis(n);
+
+        output.Should().BeEquivalentTo(expectedOutput);
+    }
+}
