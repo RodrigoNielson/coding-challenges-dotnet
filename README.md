9302eee43a2abcba189e9252ebba125cf7fd987f
maxtuist <digo_nielson@hotmail.com>
2024-05-04 14:17:32 -0300

    881. Boats to Save People

--- README.md
+++ README.md
@@ -18,4 +18,5 @@ Each solution has a corresponding test, that was created using the examples in L
 | # | Title | Difficulty |
 |---| ----- | ---------- |
 |26|[Remove Duplicates from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array/) | Easy |
+|881|[Boats to Save People](https://leetcode.com/problems/boats-to-save-people/) | Medium |
 |1122|[Relative Sort Array](https://leetcode.com/problems/relative-sort-array/) | Easy |
---
+++ src/leetcode-dotnet/Medium/BoatsToSavePeople/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_dotnet.Medium.BoatsToSavePeople;
+
+/// <see cref="https://leetcode.com/problems/boats-to-save-people/description/"/>
+public class Solution
+{
+    public int NumRescueBoats(int[] people, int limit)
+    {
+        if (people.Length == 1)
+            return 1;
+
+        Array.Sort(people);
+
+        var i = 0;
+        var j = people.Length - 1;
+        var boats = 0;
+        while (i <= j)
+        {
+            if (i == j || people[i] + people[j] <= limit)
+            {
+                i++;
+            }
+
+            j--;
+            boats++;
+        }
+
+        return boats;
+    }
+}
