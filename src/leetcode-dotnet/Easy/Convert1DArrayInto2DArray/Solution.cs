f74c135ce3fb215759586d52d4d1d5c574a028e4
maxtuist <digo_nielson@hotmail.com>
2024-05-07 17:45:48 -0300

    2022. Convert 1D Array Into 2D Array

---
+++ src/leetcode-dotnet/Easy/Convert1DArrayInto2DArray/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_dotnet.Easy.Convert1DArrayInto2DArray;
+
+/// <see cref="https://leetcode.com/problems/convert-1d-array-into-2d-array/description/"/>
+public class Solution
+{
+    public int[][] Construct2DArray(int[] original, int m, int n)
+    {
+        if (original.Length != m * n)
+            return [];
+
+        var result = new int[m][];
+
+        for (int i = 0; i < m; i++)
+        {
+            var index = i * n;
+            var finalIndex = index + n;
+            result[i] = original[index..finalIndex];
+        }
+
+        return result;
+    }
+}
