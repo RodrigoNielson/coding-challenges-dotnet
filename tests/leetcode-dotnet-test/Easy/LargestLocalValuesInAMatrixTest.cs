89d903f2619a612b3324819cb04c08ae0f46ce11
maxtuist <digo_nielson@hotmail.com>
2024-05-12 11:33:12 -0300

    2373. Largest Local Values in a Matrix

---
+++ tests/leetcode-dotnet-test/Easy/LargestLocalValuesInAMatrixTest.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using leetcode_dotnet.Easy.LargestLocalValuesInAMatrix;
+using Xunit;
+
+namespace leetcode_dotnet_test.Easy;
+public class LargestLocalValuesInAMatrixTest
+{
+    public static IEnumerable<object[]> GetData()
+    {
+        // Example 1
+        yield return new object[]
+        {
+            new int[][]
+            {
+                [9, 9, 8, 1],
+                [5, 6, 2, 6],
+                [8, 2, 6, 4],
+                [6, 2, 2, 2]
+            },
+            new int[][]
+            {
+                [9, 9],
+                [8, 6]
+            },
+        };
+
+        // Example 2:
+        yield return new object[]
+        {
+            new int[][]
+            {
+                [1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1],
+                [1, 1, 2, 1, 1],
+                [1, 1, 1, 1, 1],
+                [1, 1, 1, 1, 1]
+            },
+            new int[][]
+            {
+                [2, 2, 2],
+                [2, 2, 2],
+                [2, 2, 2]
+            },
+        };
+    }
+
+    [Theory]
+    [MemberData(nameof(GetData))]
+    public void Test(int[][] grid, int[][] expectedOutput)
+    {
+        var solution = new Solution();
+
+        var output = solution.LargestLocal(grid);
+
+        output.Should().BeEquivalentTo(expectedOutput, opt => opt.WithStrictOrdering());
+    }
+}
+
